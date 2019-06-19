b7bbab509bf30f90b2a5d26773557c49c3156b85
bt franzen <btfranzen@gmail.com>
2019-06-19 13:40:31 -0600

    added dotenv

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const request = require('request');
 const bodyParser = require('body-parser');
+
+const dotenv = require('dotenv').config();
+const creds = process.env.creds
+
 const goog = require('googleapis');
 
 // const googleData = require('./public/googleData');
-const secrets = require('./secrets')
-const key = secrets.key0;
+
 
 
 app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+app.get('/', function(req, res) {
+    console.log('creds -->  ', creds);
+    res.sendFile('./public/index.html', { root: './' });
+});
